c9eb97f53e24d0eaa85ec869f2f87e86c282af7a
pchel.21@yandex.ru <pchel.21@yandex.ru>
2024-05-30 22:12:17 +0300

    new project

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import "net/http"
+
+func hanlerPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(201)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		w.Write([]byte("Temporary Redirect"))
+		w.Header().Set("Location", "https://practicum.yandex.ru/")
+		w.WriteHeader(307)
+		return
+	}
+
+	w.WriteHeader(400)
+	return
+
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hanlerPost)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sokol2106/go-url-shortener
+
+go 1.21.1
