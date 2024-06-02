2bba6f8a57aeb8ab312034da7dff45bb866b7280
pchel.21@yandex.ru <pchel.21@yandex.ru>
2024-06-02 15:16:14 +0300

    update to storage.go

--- internal/app/shorturl.go
+++ internal/app/shorturl.go
@@ -5,13 +5,14 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	storage "github.com/sokol2106/go-url-shortener/internal/storage"
 	"io"
 	"math/rand"
 	"net/http"
 	"net/url"
 )
 
-var tableshortdata = make(map[string]Shortdata)
+var tableshortdata = make(map[string]*storage.Shortdata)
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -34,14 +35,14 @@ func HanlerMain(w http.ResponseWriter, r *http.Request) {
 
 		tshdata, exist := tableshortdata[thash]
 		if !exist {
-			tshdata = shortdata{string(body), "/" + randText(8)}
+			tshdata = storage.NewShortdata(string(body), "/"+randText(8))
 			tableshortdata[thash] = tshdata
 		}
 
 		//w.Header().Set("Location", thash)
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
-		_, _ = fmt.Fprintf(w, "http://localhost:8080%s", tshdata.short)
+		_, _ = fmt.Fprintf(w, "http://localhost:8080%s", tshdata.Short())
 
 		return
 	}
@@ -50,8 +51,8 @@ func HanlerMain(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
 		for _, value := range tableshortdata {
-			if path == value.short {
-				w.Header().Set("Location", value.url)
+			if path == value.Short() {
+				w.Header().Set("Location", value.Url())
 				w.WriteHeader(http.StatusTemporaryRedirect)
 				return
 			}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,3 +4,15 @@ type Shortdata struct {
 	url   string
 	short string
 }
+
+func NewShortdata(url string, short string) *Shortdata {
+	return &Shortdata{url, short}
+}
+
+func (sd *Shortdata) Url() string {
+	return sd.url
+}
+
+func (sd *Shortdata) Short() string {
+	return sd.short
+}
