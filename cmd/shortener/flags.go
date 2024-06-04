d7153e5256d35cf54bfc593fd74f772620b74418
pchel.21@yandex.ru <pchel.21@yandex.ru>
2024-06-04 22:09:11 +0300

    default redirect 8080

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -4,7 +4,7 @@ import "flag"
 
 func ParseFlags(baseHost *string, shortHost *string) {
 	flag.StringVar(baseHost, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(shortHost, "b", "localhost:7777", "base address and port of the resulting shortened URL")
+	flag.StringVar(shortHost, "b", "localhost:8080", "base address and port of the resulting shortened URL")
 
 	flag.Parse()
 }
