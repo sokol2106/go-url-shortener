c38baa386493e0163b223634ecb5e29f83d978e6
Pchelincev <pchel.21@yandex.ru>
2024-06-04 17:16:04 +0300

    add flags

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -1,1 +1,13 @@
 package main
+
+import "flag"
+
+var flagRunAddr string
+
+func parseFlags() {
+	// регистрируем переменную flagRunAddr
+	// как аргумент -a со значением :8080 по умолчанию
+	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	// парсим переданные серверу аргументы в зарегистрированные переменные
+	flag.Parse()
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,31 @@
 package config
 
+import "fmt"
+
 const (
 	DefaultHTTPpPort = 8080
-	DefaultHTTPHost = "localhost"
+	DefaultHTTPHost  = "localhost"
 )
+
+type configServer struct {
+	httpHost string
+	httpPort int
+}
+
+func NewConfig(h string, p int) *configServer {
+	return &configServer{
+		httpHost: h,
+		httpPort: p,
+	}
+}
+
+func (cs *configServer) HTTPHost() string {
+	return cs.httpHost
+}
+
+func (cs *configServer) HTTPPort() int {
+	return cs.httpPort
+}
+func (cs *configServer) URL() string {
+	return fmt.Sprintf("http://%s:%d", cs.HTTPHost(), cs.HTTPPort())
+}
