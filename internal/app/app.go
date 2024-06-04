3ffcfa99cd94464df341377ea89beebebf25ae43
Pchelincev <pchel.21@yandex.ru>
2024-06-04 11:38:14 +0300

    add flags.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,17 +6,6 @@ import (
 )
 
 func Run() {
-	/*servermux := http.NewServeMux()
-	servermux.HandleFunc("/", shorturl.HanlerMain)
-
-	err := http.ListenAndServe(":8080", servermux)
-	if err != nil {
-
-		panic(err)
-	}
-
-	*/
-	
 	srv := server.NewServer(shorturl.ShortRouter())
 	err := srv.Start()
 	if err != nil {
