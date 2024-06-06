1345901bb7f7dd95e172e2749edd5cf39b8a4a43
Pchelincev <pchel.21@yandex.ru>
2024-06-06 11:28:38 +0300

    add params os

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"github.com/sokol2106/go-url-shortener/internal/app"
 	"github.com/sokol2106/go-url-shortener/internal/config"
+	"os"
 )
 
-func main() {
+type params struct {
+	SERVER_ADDRESS string
+	BASE_URL       string
+}
 
+func main() {
 	var (
-		add  string
-		add2 string
+		flg  bool
+		flg2 bool
 	)
 
-	ParseFlags(&add, &add2)
-	app.Run(config.NewConfigURL(add), config.NewConfigURL(add2))
+	p := params{"localhost:8080", "localhost:8080"}
+	p.SERVER_ADDRESS, flg = os.LookupEnv("SERVER_ADDRESS")
+	p.BASE_URL, flg2 = os.LookupEnv("BASE_URL")
+
+	if !flg || !flg2 {
+		ParseFlags(&p.SERVER_ADDRESS, &p.BASE_URL)
+	}
+	
+	app.Run(config.NewConfigURL(p.SERVER_ADDRESS), config.NewConfigURL(p.BASE_URL))
 }
