d6571eaecaa6a02a2ef07c9fd857c6dd37463302
Pchelincev Ivan <53219090+sokol2106@users.noreply.github.com>
2024-06-11 08:15:12 +0300

    Revert "Iter5"

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,29 +1,3 @@
 package main
 
-import (
-	"github.com/sokol2106/go-url-shortener/internal/app"
-	"github.com/sokol2106/go-url-shortener/internal/config"
-	"os"
-)
-
-type params struct {
-	ServerAddress string
-	BaseURL       string
-}
-
-func main() {
-	var (
-		flg  bool
-		flg2 bool
-	)
-
-	p := params{"localhost:8080", "localhost:8080"}
-	p.ServerAddress, flg = os.LookupEnv("SERVER_ADDRESS")
-	p.BaseURL, flg2 = os.LookupEnv("BASE_URL")
-
-	if !flg || !flg2 {
-		ParseFlags(&p.ServerAddress, &p.BaseURL)
-	}
-
-	app.Run(config.NewConfigURL(p.ServerAddress), config.NewConfigURL(p.BaseURL))
-}
+func main() {}
