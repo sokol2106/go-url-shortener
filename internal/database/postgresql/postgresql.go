43a02efced69fc68bf8999bda8a1ad19e5295485
pchel.21@yandex.ru <pchel.21@yandex.ru>
2024-07-03 21:20:17 +0300

    1 sec

--- internal/database/postgresql/postgresql.go
+++ internal/database/postgresql/postgresql.go
@@ -52,7 +52,7 @@ func (pstg *Postgresql) Disconnect() error {
 }
 
 func (pstg *Postgresql) PingContext() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	return pstg.db.PingContext(ctx)
 }
