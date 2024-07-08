69d598cdc757f5c1bc6cf5b5d61fb30641116e0b
Pchelincev <pchel.21@yandex.ru>
2024-07-08 16:20:41 +0300

    dr files

--- internal/storage/postgresql.go
+++ internal/storage/postgresql.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/golang-migrate/migrate/v4/source/github"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"log"
