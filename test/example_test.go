7a5335d8e42e7b642cd7c98ec439b919002627a4
Pchelincev <pchel.21@yandex.ru>
2024-09-24 11:43:51 +0300

    exeanple

--- test/example_test.go
+++ test/example_test.go
@@ -20,7 +20,7 @@ func newServer() *httptest.Server {
 	return server
 }
 
-func ExamplePostGet() {
+func Example() {
 	server := newServer()
 	request, err := http.NewRequest(http.MethodPost, server.URL, strings.NewReader("https://ya.ru/"))
 	if err != nil {
@@ -48,87 +48,87 @@ func ExamplePostGet() {
 	if err != nil {
 		return
 	}
-}
 
-func ExamplePostJSON() {
-	server := newServer()
+	// PostJSON
+
 	reqBody := "{\"url\": \"https://practicum.yandex.ru\"}"
-	request, err := http.NewRequest(http.MethodPost, server.URL+"/api/shorten", strings.NewReader(reqBody))
+	request, err = http.NewRequest(http.MethodPost, server.URL+"/api/shorten", strings.NewReader(reqBody))
 	if err != nil {
 		return
 	}
 	request.Header.Set("Content-Type", "application/json")
-	response, err := server.Client().Do(request)
+	response, err = server.Client().Do(request)
 	if err != nil {
 		return
 	}
-	resBody, err := io.ReadAll(response.Body)
+	resBody, err = io.ReadAll(response.Body)
 	if err != nil {
 		return
 	}
 	defer response.Body.Close()
 	fmt.Println(string(resBody))
-}
 
-func ExamplePostBatch() {
-	server := newServer()
-	reqBody := "[{\"correlation_id\": \"1111\",\"original_url\": \"https://www.ozon.ru\"}," +
+	// PostBatch
+
+	reqBody = "[{\"correlation_id\": \"1111\",\"original_url\": \"https://www.ozon.ru\"}," +
 		"{\"correlation_id\": \"2222\",\"original_url\": \"https://ya.ru\"}]"
-	request, err := http.NewRequest(http.MethodPost, server.URL+"/api/shorten/batch", strings.NewReader(reqBody))
+	request, err = http.NewRequest(http.MethodPost, server.URL+"/api/shorten/batch", strings.NewReader(reqBody))
 	if err != nil {
 		return
 	}
-	response, err := server.Client().Do(request)
+	response, err = server.Client().Do(request)
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
 
 	fmt.Println("ExamplePostBatch")
 	fmt.Println(response.StatusCode)
-}
 
-func ExampleGetPing() {
-	server := newServer()
-	request, err := http.NewRequest(http.MethodGet, server.URL+"/ping", strings.NewReader(""))
+	// GetPing
+
+	request, err = http.NewRequest(http.MethodGet, server.URL+"/ping", strings.NewReader(""))
 	if err != nil {
 		return
 	}
-	response, err := server.Client().Do(request)
+	response, err = server.Client().Do(request)
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
+
 	fmt.Println("ExampleGetPing")
 	fmt.Println(response.StatusCode)
 
-}
+	// GetUserShortenedURLs
 
-func ExampleGetUserShortenedURLs() {
-	server := newServer()
-	request, err := http.NewRequest(http.MethodGet, server.URL+"/api/user/urls", strings.NewReader(""))
+	request, err = http.NewRequest(http.MethodGet, server.URL+"/api/user/urls", strings.NewReader(""))
 	if err != nil {
 		return
 	}
-	response, err := server.Client().Do(request)
+	response, err = server.Client().Do(request)
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
+
 	fmt.Println("ExampleGetUserShortenedURLs")
 	fmt.Println(response.StatusCode)
-}
 
-func ExampleDeleteUserShortenedURLs() {
-	server := newServer()
-	reqBody := "[\"rfgtyhju\",\"qazxswed\",\"wsxcderf\"]"
-	request, err := http.NewRequest(http.MethodDelete, server.URL+"/api/user/urls", strings.NewReader(reqBody))
+	// DeleteUserShortenedURLs
+
+	reqBody = "[\"rfgtyhju\",\"qazxswed\",\"wsxcderf\"]"
+	request, err = http.NewRequest(http.MethodDelete, server.URL+"/api/user/urls", strings.NewReader(reqBody))
 	if err != nil {
 		return
 	}
-	response, err := server.Client().Do(request)
+
+	response, err = server.Client().Do(request)
 	if err != nil {
 		return
 	}
+	defer response.Body.Close()
 
 	fmt.Println("ExampleDeleteUserShortenedURLs")
 	fmt.Println(response.StatusCode)
-
 }
